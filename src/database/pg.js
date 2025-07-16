ad23a02eebb5ecb0bb06c40a71a52877a4daad03
TeddyToT <minh.bakuryu@gmail.com>
2025-07-16 13:46:04 +0700

    Add docker-compose and Dockerfile

--- src/database/pg.js
+++ src/database/pg.js
@@ -1,18 +1,31 @@
 const { Pool } = require('pg')
-const host = process.env.PG_HOST;
-const port = process.env.PG_PORT;
-const user = process.env.PG_USER;
-const password = process.env.PG_PASSWORD;
-const database = process.env.PG_DATABASE;
+const host ='db';
+const port = process.env.PG_PORT || 5432;
+const user = process.env.PG_USER || 'postgres';
+const password = process.env.PG_PASSWORD || 'abc123';
+const database = process.env.PG_DATABASE || 'mydb';
 
+const connectionString = 'postgresql://' + user + ':' + password + '@' + host + ':' + port + '/' + database
+console.log("connect string: ", connectionString);
 
+// const dbConfig = {    
+//     host: host||'127.0.0.1',
+//     port: port||5432,
+//     user: user||'postgres',
+//     password: password||'abc123',
+//     database: database||'testdb'
+//   }
 const pool = new Pool({
-    host: host||'localhost',
-    port: port||5432,
-    user: user||'postgres',
-    password: password||'abc123',
-    database: database||'testdb'
+    // host: host||'localhost',
+    // port: port||5432,
+    // user: user||'postgres',
+    // password: password||'abc123',
+    // database: database||'testdb'
+    connectionString
+
+    // dbConfig
 })
+// console.log('Attempting to connect to PostgreSQL with config:', dbConfig);
 
 pool
   .connect()
--- src/database/product.sql
+++ src/database/product.sql
@@ -1,3 +1,15 @@
+
+CREATE TABLE IF NOT EXISTS PRODUCT
+(
+    id uuid NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    slug VARCHAR(100) NOT NULL,
+    quantity integer DEFAULT 0,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT product_unique UNIQUE (id, slug)
+);
+
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -6,19 +18,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS PRODUCT
-(
-    id uuid NOT NULL,
-    name VARCHAR(100) NOT NULL ,
-    slug VARCHAR(100) NOT NULL,
-    quantity integer DEFAULT 0,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    updated_at timestamp without time zone NOT NULL DEFAULT now(),
-    CONSTRAINT product_unique UNIQUE (id, slug)
-);
-
-
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON PRODUCT
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
+
+INSERT INTO PRODUCT (id,name, slug, quantity) VALUES 
+('9c04364b-7425-445f-a37a-c3e61af9abe8', 'Product one', 'product1', 20),
+('d1bc28aa-810e-4774-a820-b8a3f9a275cc', 'Product two', 'product2', 20),
+('bad56541-1a31-4a02-9b8e-a71b8f705af4', 'Product three', 'product3', 20),
+('bd2a5e9e-5342-4ba3-a1fc-1f9de6c9ae37', 'Product four', 'product4', 20);
